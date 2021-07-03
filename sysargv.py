1ac4f679d5a50247d54476d7e0ac7db06aec6e85
evananyonga <evewish@gmail.com>
2021-07-02 23:43:53 -0700

    add five argv flags

--- sysargv.py
+++ sysargv.py
@@ -1,46 +1,20 @@
-# A script that handles commandline arguments with sys.argv
-# import os
+# import system package
 import sys
-import glob
-import getopt
 
 
 def main():
-    output = None
-    verbose = False
-    output = 'default.out'
-    files = r'C:\Users\Ec\Downloads\Compressed\*.txt'
-    sys_args = sys.argv[1:]
-    try:
-        opts, args = getopt.getopt(sys_args, 'ho:v* .txt', ['help', 'output='])
-    except getopt.GetoptError as err:
-        err
-        sys.exit(2)
+    sys_argv = sys.argv[1:]
 
-    for flag, opt in opts:
-        if flag == '-v':
+    # loop through the sys.argv values
+    for flag in sys_argv:
+        if flag == '-help' or flag == '-h':
+            help()
+        elif flag in ('--version', '-V'):
+            print(sys.version)
+        elif flag in ('-v', '-vv'):
             verbose = True
             print(verbose)
-        elif flag in ('-h', '--help'):
-            help()
-            sys.exit()
-        elif flag in ('-o', '--output'):
-            output = opt
-            print(output)
-        elif flag in files:
-            print(glob.glob(flag))
-        else:
-            assert False, 'unhandled option'
-            # subprocess.call([r'C:\Program Files\Git\\bin\\bash.exe', flag], shell=True)
-        # if flag == '-v':
-        #     print(sys.version)
-        # elif flag == 'ls':
-        #     print(os.listdir())
-        # elif flag == 'dir':
-        #     print(os.getcwd())
-        # else:
-        #     print('Command not found')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
