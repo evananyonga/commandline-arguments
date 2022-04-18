119fa8d18a4ce711e7f54ce42183eead4fb50d9e
evananyonga <evewish@gmail.com>
2022-04-18 10:26:14 +0300

    introduce getopt

--- sysargv.py
+++ sysargv.py
@@ -1,9 +1,15 @@
 # import system package
-import sys
+import sys, getopt
 
 
 def main():
     sys_argv = sys.argv[1:]
+    input_file = ''
+    try:
+        opts, args = getopt.getopt(sys_argv, 'hVo:i:v', ['help', 'version', 'output=', 'ifile='])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
 
     # loop through the sys.argv values
     for flag in sys_argv:
